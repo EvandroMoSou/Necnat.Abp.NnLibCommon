0b791c2855c0acf664748d664629925b38d8e856
Evandro Mota de Souza <evandromosou@outlook.com>
2024-06-15 01:29:59 -0300

    0.1.7

---
+++ src/Necnat.Abp.NnLibCommon.Domain.Shared/Domains/NnIdentity/NndentityUserConsts.cs
@@ -0,0 +1,8 @@
+﻿namespace Necnat.Abp.NnLibCommon.Domains.NnIdentity
+{
+    public static class NndentityUserConsts
+    {
+        public const string UserNameDisplay = "Username";
+        public const string NameDisplay = "Name";
+    }
+}
