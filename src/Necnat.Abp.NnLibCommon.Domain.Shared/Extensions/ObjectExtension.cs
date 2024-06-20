41afe13ff734893ad52012e69c220c9261cfd975
Evandro Mota de Souza <evandromosou@outlook.com>
2024-06-20 19:10:13 -0300

    0.1.14

--- src/Necnat.Abp.NnLibCommon.Domain.Shared/Extensions/ObjectExtension.cs
+++ src/Necnat.Abp.NnLibCommon.Domain.Shared/Extensions/ObjectExtension.cs
@@ -11,5 +11,12 @@ namespace Necnat.Abp.NnLibCommon.Extensions
             serializeValue = serializeValue.Replace("[", string.Empty).Replace("]", string.Empty);
             return (DateTime?)(serializeValue == "null" ? null : JsonSerializer.Deserialize<DateTime>(serializeValue));
         }
+
+        public static DateTimeOffset? ToNullableDateTimeOffset(this object obj)
+        {
+            var serializeValue = JsonSerializer.Serialize(obj);
+            serializeValue = serializeValue.Replace("[", string.Empty).Replace("]", string.Empty);
+            return (DateTimeOffset?)(serializeValue == "null" ? null : JsonSerializer.Deserialize<DateTimeOffset>(serializeValue));
+        }
     }
 }
